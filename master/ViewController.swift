5df7d341fbd20e0b5587859c58627c8af2bf279e
JamesCookies <manilxcode@gmail.com>
2017-02-01 18:35:24 +0100

    Initial Commit

---
+++ master/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  master
+//
+//  Created by Manil Belarbi on 01/02/2017.
+//  Copyright © 2017 jamescookies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
